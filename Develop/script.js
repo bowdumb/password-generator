3ecde295257f4bfac0be81cf1ea35fd4f89b186e
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-04-06 00:15:11 -0400

    Added return statement to popUp function.

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-
+//Added variables for neccessary inputs needed to generate appropriate passwords.
 var numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l","m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "", "", "", "", "",] 
@@ -13,10 +13,13 @@ var passArray = [];
 
 function generatePassword() {
    console.log("Hey! You clicked the button!");
+   
+   alert("How many characters would you like your password to contain?");
 
-//1. Prompt the user for the password criteria
-//  a. Passworth length 8 < 128 characters
-//  b. Lowercase, uppercase, special characters
+
+//1. Prompt the user for the password criteria.
+//  a. Passworth length 8 < 128 characters.
+//  b. Lowercase, uppercase, numbers, special characters.
 //2. Validate the input. (ensure that A and B are met, and at least one character type is being selected)
 //3.Display generated password based on criteria.
 //
@@ -46,23 +49,38 @@ function generatePassword() {
 
 }
 
+//Added first line of if/else code to popUP variable.
 function popUp() {
   length = prompt("How many characters would you like your password to contain? You may select 8-128 characters");
-//Added first line of if/else code to popUP variable.
 
 if (length < 8 || length > 128) {
-  alert("Invalid character amount. Please enter a character between 8-128");
+  alert("Invalid entry. Please enter a character count between 8-128");
   console.log("Invalid character amount. Please enter a character between 8-128");
   return false;
 }
-//Added IF statement to create confirm popup box for lowercase letters.
+//Added IF statement to create confirm() popup box for lowercase letters.
 if (confirm("Would you like your password to include lowercase letters?")) {
   passArray = passArray.concat(lower);
-  console.log("You would like your password with lowercase letters!");
+  console.log("You would like your password to include lowercase letters!");
 }
-//Added IF statement to create confirm popup box for uppercase letters.
+//Added IF statement to create confirm() popup box for uppercase letters.
 if (confirm("Would you like your password to include uppercase letters?")) {
   passArray = passArray.concat(upper);
+  console.log("You would like your password to include uppercase letters!");
 }
+//Added IF statement to create confirm() popup box for numbers.
+if (confirm("Would you like your password to contain numbers?"))
+  passArray = passArray.concat(numbers);
+  console.log("You would like your password to contain numbers!");
+//Added IF statement to create confirm() popup box for special characters.
+if (confirm("Would you like your password to contain special characters?")) {
+  passArray = passArray.concat(special);
+  console.log("You would like your password to contain special characters!");
 }
-popUp();
+
+return true;
+
+}
+
+// popUp();
+
