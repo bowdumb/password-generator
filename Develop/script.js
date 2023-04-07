96a99c80ce100bbbff10d2ac7f79c5958e97b56a
Josh Claxton <125156252+bowdumb@users.noreply.github.com>
2023-04-06 23:58:56 -0400

    Added generatePassword function content and created break point in popUp function if no character type is selected

--- Develop/script.js
+++ Develop/script.js
@@ -9,14 +9,6 @@ var special = ["!", "@", "#", "$", "%", "^", "*", "&", "(", ")", ",", ".", "/", 
 var length = 8;
 var passArray = [];
 
-
-
-function generatePassword() {
-   console.log("Hey! You clicked the button!");
-   
-   alert("How many characters would you like your password to contain?");
-
-
 //1. Prompt the user for the password criteria.
 //  a. Passworth length 8 < 128 characters.
 //  b. Lowercase, uppercase, numbers, special characters.
@@ -26,63 +18,74 @@ function generatePassword() {
 
 
 
-
-
-
-  }
-
-
 // Write password to the #password input
 function writePassword() {
-  popUp();
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-  
-  passwordText.value = password;
+    
+    var truePop = popUp(); //Assigns the value of truePop to the boolean value of popUp();
+    var passwordText = document.querySelector("#password");
 
+    if (truePop) {    //If value of popUP() is true, code block below will execute.
+
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
+
+    } else{
+      passwordText.value = "Please select a valid input";
+    }
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-popUp(); //Added popUp prompt function to run when the "Generate Password" button is clicked.
-function generatePassword() {
+// popUp(); //Added popUp prompt function to run when the "Generate Password" button is clicked.
 
+
+function generatePassword() {
+  var password = "";
+    for(var i = 0; i < length; i++) {
+      var randomValue = Math.floor(Math.random() * passArray.length);
+      password = password + passArray[randomValue];
+    }
+    return password;
 }
 
 //Added first line of if/else code to popUP variable.
 function popUp() {
+  
   length = prompt("How many characters would you like your password to contain? You may select 8-128 characters");
-  passArray = [];
-if (length < 8 || length > 128) {
+  // passArray = [];
+
+
+  if (length < 8 || length > 128) {
   alert("Invalid entry. Please enter a character count between 8-128");
   console.log("Invalid character amount. Please enter a character between 8-128");
-  return false;
+  return false; //Returns a value of FALSE, and stops executing code below.
 }
-//Added IF statement to create confirm() popup box for lowercase letters.
+//Added IF statement to create confirm() popup box for lowercase letters, and concatenate it to passArray.
 if (confirm("Would you like your password to include lowercase letters?")) {
   passArray = passArray.concat(lower);
   console.log("You would like your password to include lowercase letters!");
 }
-//Added IF statement to create confirm() popup box for uppercase letters.
+//Added IF statement to create confirm() popup box for uppercase letters , and concatenate it to passArray.
 if (confirm("Would you like your password to include uppercase letters?")) {
   passArray = passArray.concat(upper);
   console.log("You would like your password to include uppercase letters!");
 }
-//Added IF statement to create confirm() popup box for numbers.
+//Added IF statement to create confirm() popup box for numbers , and concatenate it to passArray.
 if (confirm("Would you like your password to contain numbers?")) {
   passArray = passArray.concat(numbers);
   console.log("You would like your password to contain numbers!");
 }
-//Added IF statement to create confirm() popup box for special characters.
+//Added IF statement to create confirm() popup box for special characters, and concatenate it to passArray.
 if (confirm("Would you like your password to contain special characters?")) {
   passArray = passArray.concat(special);
   console.log("You would like your password to contain special characters!");
 }
 
-return true;
+else (confirm("You must select at least one character type! Please retry.")); //Requires the selection of at least one character type in order for the value of TRUE to be returned.
+
+return true; //Ends function and returns specified value to the function caller.
 
 }
-
-// popUp();
-
